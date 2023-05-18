4e086fb5c3bf82a8e43bdc7fefc219a941094080
Bartek <Barteknr1@gmail.com>
2023-05-18 20:18:49 +0200

    changed title condition

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ const Statistics = ({
     title,
     stats}) => (
     <section className={css.statistics}>
-        {!!title && <h2 className={css.title}>{title}</h2>}
+        {title && <h2 className={css.title}>{title}</h2>}
 
         <ul className={css.statList}>
             {stats.map(item => (
